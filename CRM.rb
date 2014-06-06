852a0e5f6462e06c0e8a0879a8ba970c8e2da26a
brandoshmando <brancraft@gmail.com>
2014-06-06 11:28:55 -0400

    MOve methods into CRM class

--- CRM.rb
+++ CRM.rb
@@ -1,20 +1,44 @@
-def print_main_menu
-	puts "[1] Add a new contact"
-  puts "[2] Modify an existing contact"
-  puts "[3] Delete a contact"
-  puts "[4] Display all the contacts"
-  puts "[5] Display an attribute"
-  puts "[6] Exit"
-  puts "Enter a number: "
-end
+class CRM
+attr_reader :name
+	def initialize(name)
+		@name = name
+	end
 
-def main_menu
-  print_main_menu
-  selection = gets.chomp.to_i
-  call_option(selection)  
-end
+	def print_main_menu
+		puts "[1] Add a new contact"
+	  puts "[2] Modify an existing contact"
+	  puts "[3] Delete a contact"
+	  puts "[4] Display all the contacts"
+	  puts "[5] Display an attribute"
+	  puts "[6] Exit"
+	  puts "Enter a number: "
+	end
 
-main_menu
+	def main_menu
+		puts "Welcome to the #{name}"
+	  print_main_menu
+	  selection = gets.chomp.to_i
+	  call_option(selection)  
+	end
+
+	def call_option(selection)
+		case selection
+		when 1 then add_new_contact 	
+		when 2 then modify_contact
+		when 3 then delete_contact
+		when 4 then display_all
+		when 5 then display_attribute
+		when 6 
+			puts "Goodbye!"
+			return
+		else
+			puts "Invalid input"
+			main_menu
+		end
+	end
+end
+crm = CRM.new("Roladex 3000")
+crm.main_menu
 
 class Contact
 	attr_accessor :id, :first_name, :last_name, :email, :notes
