7850233bc0f223f565411fad5582405bc142e2d6
brandoshmando <brancraft@gmail.com>
2014-06-06 15:52:24 -0400

    Adding display all contact method to roladex. Separted classes out into their own files.

---
+++ Rolodex.rb
@@ -0,0 +1,33 @@
+class Rolodex
+	attr_accessor :contacts
+	def initialize 
+		@contacts = []
+		@counter = 1000
+	end
+
+	def add_contact(contact)
+		@contacts << contact 
+	end
+
+	def find_by_id(id)
+		@contacts.find
+	end
+
+	def all 
+		if @contacts.empty? 
+			puts "-------------------------------------------"
+			puts "Your rolodex is empty. Add a contact first!"
+			puts "-------------------------------------------"
+		else
+			puts "-----------------------"
+			@contacts.each do |contact|
+				puts "First Name: #{contact.first_name}"
+				puts "Last Name: #{contact.last_name}"
+				puts "Email Address: #{contact.email}"
+				puts "Notes: #{contact.note}"
+				puts "ID: #{contact.id}"
+				puts "-----------------------"
+			end
+		end
+	end
+end
